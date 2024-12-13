ef00f8682072bd3991b36e0b4cce72fe9e6d81b5
DAbed <dabed001@doc.gold.ac.uk>
2024-12-13 09:54:05 +0000

    Update

--- create_db.sql
+++ create_db.sql
@@ -9,6 +9,12 @@ CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price D
 
 CREATE TABLE users (id INT AUTO_INCREMENT,username VARCHAR(200),firstName VARCHAR(200) ,lastName VARCHAR(200) ,email VARCHAR(200) ,hashedPassword VARCHAR(200), PRIMARY KEY(id));
 
+CREATE TABLE authors (author_id INT AUTO_INCREMENT,name VARCHAR(200), authorBio TEXT, PRIMARY KEY(author_id));
+
+CREATE TABLE bookAuthors (author_id INT, book_id INT, FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE, FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE, PRIMARY KEY (book_id, author_id));
+
+CREATE TABLE bookReviews (review_id INT AUTO_INCREMENT, user_id INT, book_id INT, rating TINYINT NOT NULL CHECK(rating >= 1 AND rating <= 5), textReview TEXT, dateReview TIMESTAMP DEFAULT, FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,  FOREIGN KEY (book_id) REFERENCES book(book_id) ON DELETE CASCADE, PRIMARY KEY(review_id));
+
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
